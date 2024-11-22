0657066961436bc3d52c40f13a61c527dd240cfe
LordZorgath <52773168+LordZorgath@users.noreply.github.com>
2024-11-22 15:39:44 -0800

    Remove extra print statement

--- src/lab5/BorrowingService.java
+++ src/lab5/BorrowingService.java
@@ -13,7 +13,6 @@ public class BorrowingService implements BorrowingServiceAPI{
 		if (book != null && m != null && m.getBorrowedBooks().contains(book)) {
 			book.setIsAvailable(true);
 			m.getBorrowedBooks().remove(book);
-			System.out.println("Returning book: " + book);
 			return true;
 		}
 		return false;
